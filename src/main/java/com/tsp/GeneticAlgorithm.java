6eb5e3f0311c9be9c1938666e67b3e9c23d686cc
vkucalo <kucalo.viktor@gmail.com>
2017-06-19 20:24:38 +0200

    Added PMX crossover

--- src/main/java/com/tsp/GeneticAlgorithm.java
+++ src/main/java/com/tsp/GeneticAlgorithm.java
@@ -3,6 +3,7 @@ package com.tsp;
 /**
  * Created by Viktor on 12.5.2017..
  */
+import java.lang.reflect.Array;
 import java.util.*;
 
 public class GeneticAlgorithm {
@@ -15,88 +16,48 @@ public class GeneticAlgorithm {
     public GeneticAlgorithm() {
 
         Scanner unos = new Scanner(System.in);
-        int N = 7;
-        City[] city = new City[7];
-        city[0] = new City(2, 2);
-        city[1] = new City(3, 3);
-        city[2] = new City(4, 4);
-        city[3] = new City(5, 5);
-        city[4] = new City(6, 6);
-        city[5] = new City(1000, 1000);
-        city[6] = new City(100, 100);
-
+        int N = 10;
+        City[] city = new City[10];
+        city[0] = new City(200, 200);
+        city[1] = new City(300, 300);
+        city[2] = new City(400, 400);
+        city[3] = new City(500, 500);
+        city[4] = new City(600, 600);
+        city[5] = new City(700, 700);
+        city[6] = new City(800, 800);
+        city[7] = new City(1000, 1000);
+        city[8] = new City(1100, 1100);
+        city[9] = new City(1110, 1110);
 
         //Initialize the population
         Population population = new Population(numberOfIndividuals, N, city);
         population.initializeRandom(numberOfIndividuals);
         best = new Individual(N, city);
         best.calculateFitness();
-        System.out.println(best.fitness);
         for(int i = 0; i < 100; i++){
-            population = crossover(city, population);
+            population = crossover(population);
             if(population.findBest().fitness > best.fitness){
                 best = population.findBest();
             }
-
             System.out.println("the best : " + best.toString() + " fitness = " + best.fitness);
         }
 
     }
 
-    public Population crossover(City[] city, Population population) {
-        //Sort the population by fitness
-        Collections.sort(population.population, new Comparator<Individual>() {
-            @Override
-            public int compare(Individual o1, Individual o2) {
-                if(o1.fitness < o2.fitness){
-                    return 1;
-                }
-                else if(o1.fitness > o2.fitness){
-                    return -1;
-                }
-                return 0;
-            }
-        });
+    public Population crossover(Population population) {
 
-        //Select the best 50% + crossover
-        Population survivors = new Population(0, population.dnaLength, city);
+        //Select the best using roullete selection
+        Population survivors = new Population(0,population.dnaLength, population.city);
+        survivors.population.addAll(rouletteSelection(population));
         for(int i = 0; i < population.population.size()/2; i+=2){
-            survivors.population.addAll(PMX(population.get(i), population.get(i+1)));
+            survivors.population.addAll(PMX(survivors.get(i), survivors.get(i+1)));
         }
-//        //Determine which positions inside the DNA will be swapped
-//        ArrayList<Integer> positionsToSwap = new ArrayList<>();
-//        int possiblePairs = city.length / 2;
-//        for(int i = 0; i < (possiblePairs + 1) * Math.random(); i++){
-//            int x = (int) (Math.random() * city.length);
-//            int y = (int) (Math.random() * city.length);
-//            positionsToSwap.add(x);
-//            positionsToSwap.add(y);
-//
-//        }
-//        Collections.shuffle(positionsToSwap);
-//        //Shuffle the DNA of the worse 50% of the population
-//        for(int i = 0; i < survivors.population.size(); i++){
-//            survivors.get(i).shuffleDna(positionsToSwap);
-//        }
         //Add the better 50% back to the survivors
-        for(int i = 0; i < population.population.size()/2; i+=2){
-            survivors.population.add(population.get(i));
-        }
         mutate(survivors.population);
         return survivors;
     }
 
 
-    public void mutate(ArrayList<Individual> prezivjeli){
-        ArrayList<Integer> positionToMutate = new ArrayList<>();
-        positionToMutate.add((int) (Math.random() * prezivjeli.get(0).dna.length));
-        positionToMutate.add((int) (Math.random() * prezivjeli.get(0).dna.length));
-        for(int i = 0; i < prezivjeli.size(); i++){
-            if(Math.random()< 0.01){
-                prezivjeli.get(i).shuffleDna(positionToMutate);
-            }
-        }
-    }
     public ArrayList<Individual> PMX(Individual parent1, Individual parent2){
         int dnaLen = parent1.dna.length;
         Individual child1 = new Individual(dnaLen, parent1.city);
@@ -122,8 +83,8 @@ public class GeneticAlgorithm {
             child2.dna[i] = -1;
         }
 
-        child1= createChild(parent1,parent2,child1,map1,from, to);
-        child2= createChild(parent2,parent1,child2,map2,from, to);
+        child1= PMXCrossover(parent1,parent2,child1,map1,from, to);
+        child2= PMXCrossover(parent2,parent1,child2,map2,from, to);
 
         ArrayList<Individual> offspring = new ArrayList<>();
         offspring.add(child1);
@@ -131,16 +92,13 @@ public class GeneticAlgorithm {
         return offspring;
 
     }
-    private Individual createChild(Individual parent1, Individual parent2, Individual child, Map map, int from, int to){
-
+    private Individual PMXCrossover(Individual parent1, Individual parent2, Individual child, Map map, int from, int to){
         for(int i = 0; i < parent1.dna.length; i++){
             int temp = parent2.dna[i];
-//            System.out.println("temp =" + temp);
             if(child.dna[i] != -1){
                 continue;
             }
             if(map.containsKey(temp)){
-//                System.out.println("sadrži temp = " + temp);
                 while(map.containsKey(temp)){
                     int tempPrev = temp;
                     temp = (int) map.get(temp);
@@ -160,17 +118,35 @@ public class GeneticAlgorithm {
                 }
             }
         }
-
         return child;
     }
-    private int checkLocation(Individual individual, int value){
-        int dnaLen = individual.dna.length;
-        for(int i = 0; i < dnaLen; i++){
-            if(individual.dna[i] == value){
-                return i;
+    public ArrayList<Individual> rouletteSelection(Population population){
+        double [] roulette = new double[population.population.size()];
+        double totalFit = 0;
+        for(Individual i: population.population){
+            totalFit += i.calculateFitness();
+        }
+        roulette[0] = population.get(0).fitness / totalFit;
+        for(int i = 1; i < population.population.size(); i++){
+            roulette[i] = roulette[i-1] + (population.get(i).fitness / totalFit);
+        }
+        Population chosen = new Population(0, population.get(0).dna.length, population.city);
+        for(int i = 0; i < population.population.size()/2; i++){
+            chosen.add(population.get(((Arrays.binarySearch(roulette, Math.random()))*(-1)) -1));
+
+        }
+
+        return chosen.population;
+    }
+    public void mutate(ArrayList<Individual> prezivjeli){
+        ArrayList<Integer> positionToMutate = new ArrayList<>();
+        positionToMutate.add((int) (Math.random() * prezivjeli.get(0).dna.length));
+        positionToMutate.add((int) (Math.random() * prezivjeli.get(0).dna.length));
+        for(int i = 0; i < prezivjeli.size(); i++){
+            if(Math.random()< 0.1){
+                prezivjeli.get(i).shuffleDna(positionToMutate);
             }
         }
-        return -1;
     }
     public static void main(String [] args){
         GeneticAlgorithm l = new GeneticAlgorithm();
